aa4dad59c8249803e3c75e55473cc10fd6d3d24a
Victor-Aienobe <viktornobe@gmail.com>
2022-03-31 22:51:14 +0100

    updates to sign in form

--- app/src/main/java/uk/ac/tees/b1636512/vikweather/MainActivity.java
+++ app/src/main/java/uk/ac/tees/b1636512/vikweather/MainActivity.java
@@ -2,14 +2,35 @@ package uk.ac.tees.b1636512.vikweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_logout;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Home");
+
+        mAuth = FirebaseAuth.getInstance();
+        btn_logout = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Toast.makeText(MainActivity.this, "Logged out successfully", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, Signin.class));
+                finish();
+            }
+        });
     }
 }
