2e63d13b528767a3c0ea83e095181c36cdc4c08e
Victor-Aienobe <viktornobe@gmail.com>
2022-04-02 20:33:06 +0100

    created cards on home activity

--- app/src/main/java/uk/ac/tees/b1636512/vikweather/MainActivity.java
+++ app/src/main/java/uk/ac/tees/b1636512/vikweather/MainActivity.java
@@ -1,9 +1,13 @@
 package uk.ac.tees.b1636512.vikweather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,7 +16,6 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_logout;
     private FirebaseAuth mAuth;
 
     @Override
@@ -20,17 +23,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Home");
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         mAuth = FirebaseAuth.getInstance();
-        btn_logout = findViewById(R.id.btn_logout);
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        switch (item.getItemId()) {
+            case R.id.logout:
                 mAuth.signOut();
                 Toast.makeText(MainActivity.this, "Logged out successfully", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(MainActivity.this, Signin.class));
                 finish();
-            }
-        });
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
